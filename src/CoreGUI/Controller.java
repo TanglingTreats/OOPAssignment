4a709df086a5d6d7464662786e9e4e4289879f08
TanglingTreats <edwintang10@gmail.com>
2020-03-15 00:01:27 +0800

    Add new items to UI

--- src/CoreGUI/Controller.java
+++ src/CoreGUI/Controller.java
@@ -11,7 +11,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
+import javafx.geometry.VPos;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -22,8 +22,14 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
 import javafx.event.ActionEvent;
+
+import DataAnalysis.Database;
+import DataAnalysis.Product;
+import DataAnalysis.Results;
+import ScraperApp.JsonFileHandler;
 
 import DataAnalysis.*;
 
@@ -39,9 +45,14 @@ public class Controller {
 
     @FXML
     private GridPane contentGrid;
+    @FXML
+    private GridPane statsGrid;
 
     @FXML
     private Text welcomeTxt;
+
+    @FXML
+    private Text invalidTxt;
 
     @FXML
     private Pane root;
@@ -54,6 +65,10 @@ public class Controller {
 
     private TextFlow popularKeywords = new TextFlow(keyword1, keyword2, keyword3);
 
+    private JsonFileHandler fileHandler;
+    private Database database;
+    private Results results;
+
     private ArrayList<Product> productArrayList = new ArrayList<Product>();
 
     private TestProduct test = new TestProduct("Thomasin McKenzie",
@@ -63,17 +78,21 @@ public class Controller {
     public void initialize() {
         System.out.println("Initialize ran!");
 
-        productArrayList.add(new TestProduct("Thomasin McKenzie",
-                "I wonder what it is",
-                "https://i.pinimg.com/originals/51/c5/5f/51c55f4a4b1166e51abd7934b2d5ce24.png"));
+//        productArrayList.add(new TestProduct("Thomasin McKenzie",
+//                "I wonder what it is",
+//                "https://i.pinimg.com/originals/51/c5/5f/51c55f4a4b1166e51abd7934b2d5ce24.png"));
+//
+//        productArrayList.add(new TestProduct("Elizabeth Olsen",
+//                "Scarlet Witch The Sexy",
+//                "https://static.tvtropes.org/pmwiki/pub/images/elizabeth_olsen_56.jpg"));
+//
+//        productArrayList.add(new TestProduct("Scarlet Johannson",
+//                "Black Widow maaaaan",
+//                "https://cdn.britannica.com/59/182359-050-C6F38CA3/Scarlett-Johansson-Natasha-Romanoff-Avengers-Age-of.jpg"));
 
-        productArrayList.add(new TestProduct("Elizabeth Olsen",
-                "Scarlet Witch The Sexy",
-                "https://static.tvtropes.org/pmwiki/pub/images/elizabeth_olsen_56.jpg"));
-
-        productArrayList.add(new TestProduct("Scarlet Johannson",
-                "Black Widow maaaaan",
-                "https://cdn.britannica.com/59/182359-050-C6F38CA3/Scarlett-Johansson-Natasha-Romanoff-Avengers-Age-of.jpg"));
+        fileHandler = new JsonFileHandler();
+        database = new Database();
+        results = new Results();
 
         createProdBox();
 
@@ -84,7 +103,7 @@ public class Controller {
 
         initialiseEventListeners();
 
-        System.out.println("Text field has this: "+searchText.getText() );
+        System.out.println("Text field has this: "+ searchText.getText() );
     }
 
     public void createProdBox() {
@@ -93,18 +112,56 @@ public class Controller {
 
     public void initialiseEventListeners()
     {
-        searchText.textProperty().addListener(((observableValue, oldValue, newValue) -> {
-            System.out.println("Textfield change from " + oldValue + " to " + newValue);
-        }));
+//        searchText.textProperty().addListener(((observableValue, oldValue, newValue) -> {
+//            System.out.println("Textfield change from " + oldValue + " to " + newValue);
+//        }));
 
         searchText.setOnKeyPressed(keyEvent -> {
             if(keyEvent.getCode() == KeyCode.ENTER) {
-                System.out.println("The input text is:" + searchText.getText());
+                //System.out.println("The input text is:" + searchText.getText());
                 System.out.println("The enter key was pressed!");
-                insertItemsIntoPane(prodPane, productArrayList);
-                setGridPane(prodPane);
-                if(welcomeTxt.isVisible()) {
-                    welcomeTxt.setVisible(false);
+
+                String input = searchText.getText().toLowerCase();
+
+                if(input.isBlank() || input.isEmpty() || input == " " || input == "") {
+                    if(!invalidTxt.isVisible()) {
+                        invalidTxt.setVisible(true);
+                    }
+                    if(welcomeTxt.isVisible()) {
+                        welcomeTxt.setVisible(false);
+                    }
+                }
+                else if(!input.isEmpty() || !input.isBlank()){
+
+                    if(invalidTxt.isVisible()) {
+                        invalidTxt.setVisible(false);
+                    }
+                    if(welcomeTxt.isVisible()) {
+                        welcomeTxt.setVisible(false);
+                    }
+                    System.out.println("Input in lowercase: " + input);
+
+                    database.compare(fileHandler.readFile(), input, results);
+
+                    Product[] top3Products = results.getTop3();
+
+                    Product[] filtered = database.filter(results.getTop3()[2].getBrand());
+
+                    for (Product x :top3Products) {
+                        System.out.println("Product name: " + x.getTitle());
+                        productArrayList.add(x);
+                        System.out.println("Image url: " + x.getPrice());
+                        System.out.println("Image url: " + x.getImage());
+                    }
+                    System.out.println("Product in array list: " + productArrayList.get(0).getTitle());
+                    System.out.println("Product in array list: " + productArrayList.get(0).getImage());
+
+                    System.out.println(results.getExpensive().getBrand());
+                    // Accept input here and pass it in to a function
+                    // convert query into lowercase letters
+                    insertItemsIntoPane(prodPane, productArrayList);
+                    setGridPane(prodPane);
+
                 }
             }
         });
@@ -125,7 +182,7 @@ public class Controller {
 
                 RowConstraints rowConst = new RowConstraints();
                 rowConst.setMaxHeight(productHeight / numberOfRows+1);
-
+                rowConst.setValignment(VPos.CENTER);
                 panel.getRowConstraints().add(rowConst);
 
             }
@@ -145,27 +202,34 @@ public class Controller {
         int counter = 0;
         for(int i = 0; i < numberOfProducts; i++) {
             for(int j = 0; j < numberOfRows; j++) {
-                Text stuff;
+                TextFlow newTitle;
+                Text toPushText;
 
                 switch (j)
                 {
                     case 0:
-                        Image image = new Image(testProducts.get(counter).getImage(), 0, productHeight / numberOfRows-1, true, true, true);
-                        ImageView iv1 = new ImageView();
-                        iv1.setImage(image);
-
-                        System.out.println("Image URL: " + testProducts.get(counter).getImage());
-                        prodPane[i].add(iv1, 0, j);
+//                        if(testProducts.get(counter).getImage() != null || !testProducts.get(counter).getImage().isEmpty() || !testProducts.get(counter).getImage().isBlank()) {
+//                            Image image = new Image(testProducts.get(counter).getImage(), 0, productHeight / numberOfRows-1, true, true, true);
+//                            ImageView iv1 = new ImageView();
+//                            iv1.setImage(image);
+//
+//                            System.out.println("Image URL: " + testProducts.get(counter).getImage());
+//                            prodPane[i].add(iv1, 0, j);
+//                        }
 
                         break;
                     case 1:
-                        stuff = new Text(testProducts.get(counter).getTitle());
+                        toPushText = new Text(testProducts.get(counter).getTitle());
+                        newTitle = new TextFlow(toPushText);
+                        newTitle.setTextAlignment(TextAlignment.CENTER);
 
-                        prodPane[i].add(stuff, 0, j);
+                        prodPane[i].add(newTitle, 0, j);
                         break;
                     case 2:
-                        stuff = new Text(testProducts.get(counter).getDesc());
-                        prodPane[i].add(stuff, 0, j);
+                        toPushText = new Text(String.format("$%.2f", testProducts.get(counter).getPrice()));
+                        newTitle = new TextFlow(toPushText);
+                        newTitle.setTextAlignment(TextAlignment.CENTER);
+                        prodPane[i].add(newTitle, 0, j);
                         break;
                 }
 
